3f742e0599168443614d48709a460db208a4822b
Matheus Gomes <matheusgomesdr@gmail.com>
2026-05-05 22:41:01 -0300

    subindo arquivos que nao tinham sido salvos

--- src/main/java/com/nines/logica/EstruturaDeRepeticao.java
+++ src/main/java/com/nines/logica/EstruturaDeRepeticao.java
@@ -1,4 +1,47 @@
 package com.nines.logica;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EstruturaDeRepeticao {
-}
+
+    public static void main(String[] args) {
+        //listadePalavras();
+        //forClassico();
+        inverteString();
+
+    }
+
+    private static void listadePalavras() {
+        List<String> palavraList = new ArrayList<>();
+        palavraList.add("Zumba");
+        palavraList.add("Games");
+        palavraList.add("Jose");
+        palavraList.add("Lucas");
+
+        palavraList.forEach(palavra -> System.out.println(palavra));
+
+    }
+    private static void forClassico() {
+        ArrayList<String> palavraList = new ArrayList<>();
+        palavraList.add("Zumba");
+        palavraList.add("Games");
+        palavraList.add("Jose");
+        palavraList.add("Lucas");
+
+        for (int posicao = 1; posicao < palavraList.size(); posicao++) {
+            System.out.println(palavraList.get(posicao));
+
+        }
+    }
+    private static void inverteString() {
+        String normal = "12345";
+
+        String invertida = "";
+
+        for (int posicao = normal.length() -1; posicao>= 0 ; posicao = posicao -1) {
+            invertida = invertida + normal.charAt(posicao);
+        }
+        System.out.println(invertida);
+    }
+    }
--- src/main/java/com/nines/logica/RepeticaoEDecisaoJuntos.java
+++ src/main/java/com/nines/logica/RepeticaoEDecisaoJuntos.java
@@ -1,4 +1,30 @@
 package com.nines.logica;
 
+import java.util.ArrayList;
+
 public class RepeticaoEDecisaoJuntos {
+    public static void main(String[] args) {
+        ArrayList<Integer> listaDeUmADez = new ArrayList<Integer>();
+
+        for (int posicao = 1; posicao <= 10; posicao++) {
+            listaDeUmADez.add(posicao);
+        }
+
+        ArrayList<Integer> numerosPares = new ArrayList<>();
+        ArrayList<Integer> numerosImpares = new ArrayList<>();
+
+        for(Integer numero : listaDeUmADez){
+            if(numero%2==0){
+                numerosPares.add(numero);
+            }else {
+                numerosImpares.add(numero);
+            }
+        }
+        for(Integer numeroPar : numerosPares){
+            System.out.println(numeroPar);
+        }
+        for(Integer numeroImpar : numerosImpares){
+            System.out.println(numeroImpar);
+        }
+    }
 }
